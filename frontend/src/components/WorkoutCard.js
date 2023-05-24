b30aca74499e08b429a0846b04978b8a099b4670
Murshid Bin Ibrahim <murshidshazz@gmail.com>
2023-05-24 17:12:32 +0530

    Delete Workout Data Implemented

--- frontend/src/components/WorkoutCard.js
+++ frontend/src/components/WorkoutCard.js
@@ -1,6 +1,34 @@
 import React from "react";
+import { useWorkoutContext } from "../hooks/useWorkoutContext";
 
 const WorkoutCard = ({ workout }) => {
+  const { dispatch } = useWorkoutContext();
+
+  const handleClick = async () => {
+    try {
+      const response = await fetch(
+        "http://localhost:8000/api/workouts/" + workout._id,
+        {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      const json = await response.json();
+
+      if (response.ok) {
+        dispatch({
+          type: "DELETE_WORKOUT",
+          payload: json,
+        });
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <div className="workout-card">
       <h4>{workout?.title}</h4>
@@ -11,6 +39,7 @@ const WorkoutCard = ({ workout }) => {
         <strong>Reps: </strong> {workout?.reps}
       </p>
       <p>{workout.createdAt}</p>
+      <span onClick={handleClick}>Delete</span>
     </div>
   );
 };
--- frontend/src/context/WorkoutContext.js
+++ frontend/src/context/WorkoutContext.js
@@ -14,6 +14,11 @@ export const workoutReducer = (state, action) => {
     case "CREATE_WORKOUT":
       return { workouts: [action.payload, ...state.workouts] };
 
+    case "DELETE_WORKOUT":
+      return {
+        workouts: state.workouts.filter((w) => w._id !== action.payload._id),
+      };
+
     default:
       return state;
   }
